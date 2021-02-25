62f96fdc370c8e5fd92b144aea9e9349cfdcfbaa
Will Hsu <whsu@ucsd.edu>
2021-02-25 09:47:37 -0800

    completed lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'E95cJaQkB7gIsLA1O1oJdcguyu6VBM5B';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.877651, -117.240044],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
